0e49861dc0e606527d0268af6583d82676643083
Bradley S. Isenbek <bisenbek@gmail.com>
2025-07-17 15:23:50 -0400

    Stuff

--- protoc/reset.sh
+++ protoc/reset.sh
@@ -10,19 +10,20 @@ function hw_reset() {
 function init_mon() {
 	local DEVICE=( $(./wifi-monitor-check | grep MONITOR | head -n1) )
 
-	sudo iw dev ${DEVICE} interface add mon0 type monitor
-	sudo ip link set mon0 up
-	sudo iw dev mon0 set channel 6
+	if [[ ! -z ${DEVICE} ]]; then
+		sudo ./wifi-monitor-setup "${DEVICE}"
+	fi
 
-	#if [[ ! -z ${DEVICE} ]]; then
-	#	sudo ./wifi-monitor-setup "${DEVICE}"
-	#fi
+	#sudo iw dev ${DEVICE} interface add mon0 type monitor
+	#sudo ip link set mon0 up
+	#sudo iw dev mon0 set channel 6
+
 }
 
 function ap_test() {
 	sudo aireplay-ng --test mon0
 }
+hw_reset
+init_mon
 ap_test
-#hw_reset
-#init_mon
 
