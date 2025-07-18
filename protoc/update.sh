0ce1ca7433597cfa3729d67e256dbd5b21d6292f
Bradley S. Isenbek <bisenbek@gmail.com>
2025-07-17 22:56:57 -0400

    stuff

--- protoc/update.sh
+++ protoc/update.sh
@@ -1,24 +1,16 @@
 #!/bin/bash
+
 sudo apt-get update
 sudo apt-get upgrade -y
 sudo apt autoremove
-python -m pip install --upgrade pip
 
-sudo apt-get install libssl-dev
-sudo apt-get install openssl
-sudo apt-get install libtool
-sudo apt-get install libtool
-sudo apt-get install openssl
-sudo apt-get install libopenssl
-sudo apt-get install libgnutls-openssl27
-sudo apt-get install openvpn
-sudo apt-get install network-tools build-essential
-sudo apt-get install nbuild-essential
-sudo apt-get install vim
-sudo apt-get install git
-sudo apt-get install build-essential
-sudo apt-get install openssh-server
+sudo apt-get install -y build-essential autoconf automake libtool pkg-config libnl-3-dev libnl-genl-3-dev libssl-dev ethtool shtool rfkill zlib1g-dev libpcap-dev libsqlite3-dev libpcre2-dev libhwloc-dev libcmocka-dev hostapd wpasupplicant tcpdump screen iw usbutils expect openssl libgnutls-openssl27 openvpn net-tools build-essential vim git build-essential openssh-server
 
 git pull
 git config --global user.email "bisenbek@gmail.com"
 git config --global user.name "Bradley S. Isenbek"
+
+. /home/bisenbek/.pyenv/versions/tinmac/bin/activate
+python -m pip install --upgrade pip
+
+sudo ldconfig
