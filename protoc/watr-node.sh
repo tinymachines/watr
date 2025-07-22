745a9c5bf071dc1bdb028d62378c328cd7268c3b
Bradley S. Isenbek <bisenbek@gmail.com>
2025-07-22 21:28:59 +0100

    Use correct MAC

--- protoc/watr-node.sh
+++ protoc/watr-node.sh
@@ -1,4 +1,4 @@
 #/bin/bash
 
 source ${WATR_ROOT}/watr-header.sh
-sudo -E PYTHONPATH=".:${WATR_ROOT}/scapy" $(which python) WATRNode.py ${WATR_DEVICE} "00:11:22:33:44:55"
+sudo -E PYTHONPATH=".:${WATR_ROOT}/scapy" $(which python) WATRNode.py ${WATR_DEVICE} ${WATR_ADDR}
