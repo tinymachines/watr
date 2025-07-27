eb4a09c423c1fb1b0037c82efd674860bf784731
Bradley S. Isenbek <bisenbek@gmail.com>
2025-07-27 20:28:47 +0100

    Logging

--- protoc/net/watr-llm.sh
+++ protoc/net/watr-llm.sh
@@ -1,4 +1,7 @@
 #/bin/bash
 
 source ${WATR_ROOT}/watr-header.sh
-sudo -E PYTHONPATH=".:${WATR_ROOT}/scapy" $(which python) llm_social_main.py ${WATR_DEVICE} ${WATR_ADDR} "${WATR_NAME}" "${WATR_LLM}"
+
+sudo -E PYTHONPATH=".:${WATR_ROOT}/scapy" \
+	$(which python) logged_llm_social_main.py \
+	${WATR_DEVICE} ${WATR_ADDR} "${WATR_NAME}" "${WATR_LLM}"
